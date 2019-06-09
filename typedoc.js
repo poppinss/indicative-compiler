d0dc86b6a249720eb9149301c02a82b966ef5976
Harminder virk <virk.officials@gmail.com>
2019-06-09 14:00:12 +0530

    docs: remove fixtures from typedoc

--- typedoc.js
+++ typedoc.js
@@ -2,7 +2,7 @@ module.exports = require('@adonisjs/mrm-preset/_typedoc.js')({
   exclude: [
     '**/test/*.ts',
     '**/test-helpers/*.ts',
-    '**/fixtures/*.ts',
+    '**/fixtures/**/*.ts',
     'index.ts',
   ],
   readme: 'none',
